d0f1dfc2d4f57e6021054d436207ade4b07219bd
rirts <rafartejadasegura@gmail.com>
2025-11-27 11:07:49 -0600

    Simplify time_spine_daily to single select for SQLFluff

--- dbt_project/models/marts/olist/time_spine_daily.sql
+++ dbt_project/models/marts/olist/time_spine_daily.sql
@@ -1,22 +1,13 @@
 {{ config(
     materialized = 'table'
 ) }}
-with base_dates as (
+select
+    cast(date_day as date) as date_day
+from
     {{
         dbt.date_spine(
             'day',
             "to_date('2016-01-01','YYYY-MM-DD')",
             "to_date('2018-12-31','YYYY-MM-DD')"
         )
-    }}
-),
-
-final as (
-    select
-        cast(date_day as date) as date_day
-    from base_dates
-)
-
-select
-    date_day
-from final;
+    }} as base_dates;
